7f7e837dc833e2d62a242cefa9626260ad60f6b7
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 10:17:52 -0300

    Organização da estrutura de pacotes

---
+++ notifications-service/src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,17 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('Should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+  });
+});
