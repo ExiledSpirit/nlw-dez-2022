91d48dcbae926a54a179f06f279534bbb6da41ab
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 18:04:12 -0300

    Adicionando caso de uso de listar notificações por recipient ID

--- notifications-service/test/repositories/in-memory-notifications-repository.ts
+++ notifications-service/test/repositories/in-memory-notifications-repository.ts
@@ -12,6 +12,10 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
     return notification;
   }
 
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter((notification) => notification.recipientId === recipientId);
+  }
+
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter((notification) => notification.recipientId === recipientId).length;
   }
