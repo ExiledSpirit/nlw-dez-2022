80683c799267bc1b089cd64d67e9ac86e5be4ae9
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 19:45:31 -0300

    Implementando o repositório do Prisma (Tal como foi feito com o in-memory-repository)

--- notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,8 +8,36 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 export class PrismaNotificationsRepositories implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
-  findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prismaService.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if (!notification) return null;
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prismaService.notification.findMany({
+      where: {
+        id: recipientId,
+      },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prismaService.notification.count({
+      where: {
+        recipientId,
+      },
+    });
+
+    return count;
   }
 
   async create(notification: Notification): Promise<void> {
@@ -20,7 +48,14 @@ export class PrismaNotificationsRepositories implements NotificationsRepository 
     });
   }
 
-  save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+  async save(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prismaService.notification.update({
+      where: {
+        id: raw.id,
+      },
+      data: raw,
+    });
   }
 }
