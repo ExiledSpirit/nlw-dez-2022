975fa087d8f959f4669331065e5481c4552dd7ac
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 17:31:19 -0300

    Adicionando caso de uso de cancelar uma notificação

--- notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -8,11 +8,19 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 export class PrismaNotificationsRepositories implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
+  findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
