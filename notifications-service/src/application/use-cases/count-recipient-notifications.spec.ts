15840ba95999baa7bea37159ceccdc39b71a227a
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 17:58:45 -0300

    Adicionando factory pattern para abstrair a instanciação de entidades similares

--- notifications-service/src/application/use-cases/count-recipient-notifications.spec.ts
+++ notifications-service/src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,5 +1,4 @@
-import { Content } from '@application/entities/content';
-import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CountRecipientNotifications } from './count-recipient-notifications';
 
@@ -8,29 +7,11 @@ describe('Count recipient by Notifications', () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const countRecipientNotifications = new CountRecipientNotifications(notificationsRepository);
 
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade!'),
-        recipientId: 'recipient-2',
-      }),
-    );
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }));
 
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade!'),
-        recipientId: 'recipient-1',
-      }),
-    );
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-1' }));
 
-    await notificationsRepository.create(
-      new Notification({
-        category: 'social',
-        content: new Content('Nova solicitação de amizade!'),
-        recipientId: 'recipient-2',
-      }),
-    );
+    await notificationsRepository.create(makeNotification({ recipientId: 'recipient-2' }));
 
     const { count } = await countRecipientNotifications.execute({
       recipientId: 'recipient-1',
