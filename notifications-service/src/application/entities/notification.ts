4fe7aa346bac29f6a51c2c64456014dbb80170f2
exiledspirit <gab.gabriel.1002@outlook.com>
2022-12-18 18:09:11 -0300

    Adicionando caso de uso de ler uma  notificação por recipient ID

--- notifications-service/src/application/entities/notification.ts
+++ notifications-service/src/application/entities/notification.ts
@@ -51,8 +51,12 @@ export class Notification {
     return this.props.content;
   }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unRead() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
